b94561d2e91e0cbe7ff29e1ad31a7f01cfc59c86
Allen Kim <allenkim67@gmail.com>
2015-08-22 23:13:09 -0700

    add routed links to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router';
 
 export default class Nav extends Component {
   render() {
     return (
       <ul>
-        <li>Browse Users</li>
+        <li><Link to="/users">Browse Users</Link></li>
         <li>
           Recent Chats
           <ul>
-            <li>tom</li>
+            <li><Link to="/chat/tom">tom</Link></li>
           </ul>
         </li>
       </ul>
