f967b5b65a2644664c8941b0de3332d763dc0ac9
Allen Kim <allenkim67@gmail.com>
2015-08-26 22:48:23 -0700

    add isSubObject function to util

--- src/util.js
+++ src/util.js
@@ -1,3 +1,9 @@
 export function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+export function isSubObject(obj1, obj2) {
+  return Object.keys(obj2).every(key => {
+    return obj1[key] === obj2[key];
+  });
+}
