bc23711eb7e4588f47c4649658817a7187cfeea4
Allen Kim <allenkim67@gmail.com>
2015-09-20 10:53:39 -0700

    fix reversed recent convos list

--- src/client/components/Nav/Nav.js
+++ src/client/components/Nav/Nav.js
@@ -1,16 +1,21 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router';
+import {deepClone} from '../../../util';
 
 export default class Nav extends Component {
   render() {
     const recentChat = (recent) => {
       const chatUrl = `/chat/${recent.username}`;
-      return (<li key={recent._id} className={recent.unread ? "js-recent-unread" : ""}><Link to={chatUrl}>{recent.username}</Link></li>);
+      return (
+        <li key={recent._id} className={recent.unread ? "js-recent-unread" : ""}>
+          <Link to={chatUrl}>{recent.username}</Link>
+        </li>
+      );
     };
 
     const recentChats = (
       <ul>
-        {this.props.currentUser.recentChats.map(recentChat)}
+        {deepClone(this.props.currentUser.recentChats).reverse().map(recentChat)}
       </ul>
     );
 
