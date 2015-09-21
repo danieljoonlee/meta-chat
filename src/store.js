4820cae49c49d2489784ef9ae5ba7c9281c7325e
Allen Kim <allenkim67@gmail.com>
2015-09-20 18:01:52 -0700

    add redux logger

--- src/store.js
+++ src/store.js
@@ -1,11 +1,18 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
 import thunkMiddleware from 'redux-thunk';
+import loggerMiddleware from 'redux-logger';
 import userBrowserReducer from './client/reducers/userBrowser';
 import sessionReducer from './client/reducers/session';
 import chatReducer from './client/reducers/chat';
 
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware, promiseMiddleware)(createStore);
+const middlewares = [
+  thunkMiddleware,
+  promiseMiddleware,
+  loggerMiddleware
+];
+
+const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
 
 const reducer = combineReducers({
   userBrowser: userBrowserReducer,
